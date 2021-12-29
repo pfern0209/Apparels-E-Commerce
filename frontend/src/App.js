2c6426fd52cd0a6c7ebb6b34d1af675ff9a16de8
Preston <prestonfernandes1234@gmail.com>
2021-12-29 11:43:05 +0530

    Completed Front-End

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,36 @@
-
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import {Container} from "react-bootstrap"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen';
 
 const App= () => {
   return (
-    <>
-      <h1>Welcome to Apparels</h1>
-    </>
+    <Router>
+    <Header/>
+    <main className="py-3">
+    <Container>
+      <Routes>
+        <Route exact path="/" element={
+          <>
+            <HomeScreen/>
+          </>
+        }>  
+        </Route>
+
+        <Route path="/product/:id" element={
+          <>
+            <ProductScreen/>
+          </>
+        }>  
+        </Route>
+      </Routes>
+
+    </Container>     
+    </main>
+    <Footer/>  
+    </Router>
   );
 }
 
