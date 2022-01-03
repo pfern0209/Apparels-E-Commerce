eb8762596322d53bf1102191133d8e1b7c8f1d7e
Preston <prestonfernandes1234@gmail.com>
2022-01-03 12:33:30 +0530

    Added Top Products Carousel for homescreen

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getProductById,getProducts,deleteProduct,createProduct,updateProduct,createProductReview } from "../controllers/productController.js";
+import { getProductById,getProducts,deleteProduct,createProduct,updateProduct,createProductReview,getTopProducts } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router=express.Router()
@@ -7,6 +7,7 @@ const router=express.Router()
 
 router.route('/').get(getProducts).post(protect,admin,createProduct)
 router.route('/:id/reviews').post(protect,createProductReview)
+router.get('/top',getTopProducts)
 
 router.route('/:id').get(getProductById).delete(protect,admin,deleteProduct).put(protect,admin,updateProduct)
 
