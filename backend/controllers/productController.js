6f066c25df0ec4ed5bf36ec55e8dc4bf4f8bd3dd
Preston <prestonfernandes1234@gmail.com>
2022-01-22 16:21:30 +0530

    Fixed add to cart bug

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -140,7 +140,6 @@ const createProductReview=asyncHandler(async(req,res)=>{
 //@access Public
 const getTopProducts=asyncHandler(async(req,res)=>{
   const products=await Product.find({}).sort({rating:-1}).limit(3)
-
   res.json(products)
 })
 
